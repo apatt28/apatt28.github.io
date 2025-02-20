c496ae4d431f079550354a6d09e1af91f2fabab9
apatt28 <120226894+apatt28@users.noreply.github.com>
2025-02-20 15:24:49 -0800

    delete all files

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.getElementById('mobile-menu');
-    const navLinks = document.getElementById('nav-links');
-
-    menuToggle.addEventListener('click', function() {
-        navLinks.classList.toggle('showing');
-    });
-});
